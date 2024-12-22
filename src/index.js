a5984bb162fb205b5b05f1bfec2e59f160b00143
Yash Ghoda <yashaghoda@gmail.com>
2024-12-22 15:53:25 +0530

    middlewares and apiErrors

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ dotenv.config({
 })
 
 connectDB()
+
+.then(() =>{
+    app.listen(process.env.PORT || 8000,() =>{
+        console.log(`Server running at port:${process.env.PORT}`)
+    })
+})
+.catch((err) =>{
+    console.log("MONGODB connection failed:",err)
+})
