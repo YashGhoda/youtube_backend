041b21043bb52bc40296e3ce7489a4e9fa6033e6
Yash Ghoda <yashaghoda@gmail.com>
2025-06-11 14:15:02 +0530

    access token and refresh token endpoint route updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError} from "../utils/ApiErrors.js"
 import { User } from "../models/user.model.js"
 import { fileUpload } from "../utils/fileupload.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-
+import jwt from 'jsonwebtoken';
 
 const generateAccessAndRefreshTokens = async(userId) =>{
     try {
@@ -161,5 +161,48 @@ const logoutUser = asyncHandler(async(req,res) =>{
 
 })
 
+const refreshAccessToken = asyncHandler(async(req,res) =>{
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-export {registerUser,loginUser,logoutUser}
+    if(!incomingRefreshToken){
+        throw new ApiError(401,"Unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(
+            incomingRefreshToken,
+            process.env.REFRESH_TOKEN_SECRET,
+        )
+    
+        const user = await User.findById(decodedToken?._id)
+    
+        if(!user){
+            throw new ApiError(401,"Invalid refresh Token")
+        }
+    
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401,"Refresh Token is expired or used")
+        }
+    
+        const options = {
+            httpOnly:true,
+            secure:true
+        }
+    
+        const {accessToken,newrefreshToken} = await generateAccessAndRefreshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken",accessToken,options)
+        .cookie("refreshToken",newrefreshToken,options)
+        .json(new ApiResponse(200,{
+            accessToken,refreshToken:newrefreshToken
+        },"Access token refreshed"))
+    } catch (err) {
+        throw new ApiError(401,error?.message || "Invalid refresh Token")
+    }
+
+})
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken}
